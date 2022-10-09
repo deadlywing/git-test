d673cf30a477efee3f25540de13650a1e45dc8bd
deadlywing <437743165@qq.com>
2022-10-09 14:40:57 +0800

    c2

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
     @Project: git-test
     @File: main.py
     @Author: zjj
-    @Date: 2022/10/9 14:24
+    @Date: 2022/10/9 14:18
 """
+import tempfile
 import numpy as np
 
+print("hello")
 print("hello world")
